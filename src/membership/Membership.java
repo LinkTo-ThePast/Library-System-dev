18fe5a3a1df2617da526e7f6c5591c61439f92ae
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2024-12-07 17:36:58 -0600

    edit membership class

--- src/membership/Membership.java
+++ src/membership/Membership.java
@@ -3,6 +3,10 @@ package membership;
 
 public class Membership {
 	
-	private String name;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private int age;
+	private String membershipType;
 	
 }
