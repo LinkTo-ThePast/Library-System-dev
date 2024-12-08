69034726fa7dda8570fd575d80c9cc15c56dcc82
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2024-12-08 17:48:14 -0600

    first method

--- src/membership/Membership.java
+++ src/membership/Membership.java
@@ -1,12 +1,32 @@
 package membership;
 
+import java.util.Arrays;
 
 public class Membership {
 	
-	private String firstName;
-	private String lastName;
-	private String email;
-	private int age;
+	// attributes: ---> basic info for clients/readers
+	private static String firstName;
+	private static String lastName;
+	private static String email;
+	private static int age;
 	private String membershipType;
+	 
+	// methods:
 	
+	// 1. retrieve a book or books from the library;
+	
+	private int bookToRead() {
+		int maxBooks = 0; // --> initialize variable, then return, not define again;
+		if(membershipType == "regular") {
+			return maxBooks = 2;
+		}
+		else if(membershipType == "student") {
+			return maxBooks = 3;
+		}
+		else if(membershipType == "constant client") {
+			return maxBooks = 4;
+		}
+		return maxBooks;
+	}
 }
+
